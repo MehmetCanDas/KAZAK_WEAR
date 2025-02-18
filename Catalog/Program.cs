b5435c68d646554145f3f0d2fca3f368520c9638
Emin <hakanmehmet849@gmail.com>
2025-02-18 23:27:17 +0300

    small update

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -8,13 +8,30 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 7126;
+});
+
+/*builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(7126, listenOptions =>
+    {
+        listenOptions.UseHttps("Keys/localhost.pem", "Keys/localhost-key.pem");
+    });
+});*/
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
-builder.Services.AddScoped<IDatabaseSettings>(sp => { 
-    
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
     return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -31,6 +48,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll"); // Activate CORS
 
 app.UseAuthorization();
 
